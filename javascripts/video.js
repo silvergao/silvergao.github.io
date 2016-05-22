068a5173748b49dfecb738d5ab311180e80ead45
kuang.zukai <345106552@qq.com>
2016-05-23 01:14:55 +0800

    Finish upper bug Done!! cause tag must be <tag></tag> but not <tag /> or jquery would see <tag />abc => <tag>abc</tag> and make errors

--- javascripts/video.js
+++ javascripts/video.js
@@ -2,7 +2,7 @@ var tagname = "tcvideo"
 var data1 = '<div class="myvideo-fluid mywidescreen myyouku"><div><iframe class="video_iframe" style="z-index:1;" src="'
 var data2 = '" allowfullscreen="" frameborder="0" width="100%" height="100%"></iframe></div></div>'
 $(document).ready(function() {
-
+	//这种用法，tag必须按照html5规则：<tag></tag> 而不是 <tag />，否则下面的代码和预想的不一样！！
 	$(tagname).each(function(i,val){
 		console.log(i);
 		console.log(val);
