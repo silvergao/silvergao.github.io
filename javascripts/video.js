63c8337d6c903d96213049f1c4b52d500f5ce16a
kuang.zukai <345106552@qq.com>
2016-05-29 13:53:43 +0800

    add scrollLoading, now test

--- javascripts/video.js
+++ javascripts/video.js
@@ -2,15 +2,19 @@ var tagname = "tcvideo"
 var data1 = '<div class="myvideo-fluid mywidescreen myyouku"><div><iframe class="video_iframe" style="z-index:1;" src="'
 var data2 = '" allowfullscreen="" frameborder="0" width="100%" height="100%"></iframe></div></div>'
 $(document).ready(function() {
-	//这种用法，tag必须按照html5规则：<tag></tag> 而不是 <tag />，否则下面的代码和预想的不一样！！
-	$(tagname).each(function(i,val){
-		var src = $(val).attr("src") + "&auto=0"
-		$(val).replaceWith(data1 + src + data2);
+	//滚动加载
+	$(tagname).scrollLoading({
+		callback: function() {
+			var src = $(this).attr("src") + "&auto=0";
+			var newHtml = data1 + src + data2;
+			$(this).replaceWith(newHtml);
+		}
 	});
 	
-    var initVideoSrc = function(obj, jp) {
-	$(obj).append("如果无法观看视频, 请点击<a href=\"" + baseurl + $(jp).attr("url") +"\">在线观看</a>" +
-	"(或点击<a href=\"" + rawurl + $(jp).attr("url") +"\">下载</a>)");
-    };
-
+	//这种用法，tag必须按照html5规则：<tag></tag> 而不是 <tag />，否则下面的代码和预想的不一样！！
+	// $(tagname).each(function(i,val){
+		// var src = $(val).attr("src") + "&auto=0"
+		// $(val).replaceWith(data1 + src + data2);
+	// });
+	
 });
