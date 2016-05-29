fa613793327944158c05a4b69c5bb36d3d55e661
kuang.zukai <345106552@qq.com>
2016-05-29 11:41:59 +0800

    remove console.log cause not all browser support it

--- javascripts/video.js
+++ javascripts/video.js
@@ -4,12 +4,8 @@ var data2 = '" allowfullscreen="" frameborder="0" width="100%" height="100%"></i
 $(document).ready(function() {
 	//这种用法，tag必须按照html5规则：<tag></tag> 而不是 <tag />，否则下面的代码和预想的不一样！！
 	$(tagname).each(function(i,val){
-		console.log(i);
-		console.log(val);
 		var src = $(val).attr("src") + "&auto=0"
-		console.log(src);
 		$(val).replaceWith(data1 + src + data2);
-		console.log($(val));
 	});
 	
     var initVideoSrc = function(obj, jp) {
